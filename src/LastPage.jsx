d07fa2091b21749c8f50157f4d0c67d6cccafa0f
Adityaa401 <adityaakr2979@gmail.com>
2025-07-26 01:40:59 +0530

    Update LastPage.jsx

--- src/LastPage.jsx
+++ src/LastPage.jsx
@@ -32,7 +32,6 @@ const LastPage = () => {
       <video
         preload="auto"
         autoPlay
-        loop
         muted
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
